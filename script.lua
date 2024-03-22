ef01631b8d136ec187b691ac9be572ab33395e1c
Bemplia <145816318+Bemplia@users.noreply.github.com>
2024-03-22 15:25:03 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -46,10 +46,8 @@ MainSection:NewButton("FullBright", "", function()
 end)
 
 MainSection:NewButton("Camera Hack", "", function()
-while game:GetService("RunService").RenderStepped:wait() do
-    plr.CameraMaxZoomDistance = 9999
-    plr.DevCameraOcclusion = Inviscam
-end
+plr.CameraMaxZoomDistance = 9999
+plr.DevCameraOcclusion = Inviscam
 end)
 
 MainSection:NewToggle("Freeze The Monster", "WOW", function(s)
@@ -71,6 +69,49 @@ MainSection:NewToggle("Freeze The Monster", "WOW", function(s)
         game:GetService("Workspace").Monster["Left Leg"].Anchored = false
         game:GetService("Workspace").Monster["Right Leg"].Anchored = false
     end
+end)
+
+MainSection:NewButton("Anti lag", "", function()
+    local Terrain = workspace:FindFirstChildOfClass('Terrain')
+	Terrain.WaterWaveSize = 0
+	Terrain.WaterWaveSpeed = 0
+	Terrain.WaterReflectance = 0
+	Terrain.WaterTransparency = 0
+	Lighting.GlobalShadows = false
+	Lighting.FogEnd = 9e9
+	settings().Rendering.QualityLevel = 1
+	for i,v in pairs(game:GetDescendants()) do
+		if v:IsA("Part") or v:IsA("UnionOperation") or v:IsA("MeshPart") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+			v.Material = "Plastic"
+			v.Reflectance = 0
+		elseif v:IsA("Decal") then
+			v.Transparency = 1
+		elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+			v.Lifetime = NumberRange.new(0)
+		elseif v:IsA("Explosion") then
+			v.BlastPressure = 1
+			v.BlastRadius = 1
+		end
+	end
+	for i,v in pairs(Lighting:GetDescendants()) do
+		if v:IsA("BlurEffect") or v:IsA("SunRaysEffect") or v:IsA("ColorCorrectionEffect") or v:IsA("BloomEffect") or v:IsA("DepthOfFieldEffect") then
+			v.Enabled = false
+		end
+	end
+	workspace.DescendantAdded:Connect(function(child)
+		task.spawn(function()
+			if child:IsA('ForceField') then
+				RunService.Heartbeat:Wait()
+				child:Destroy()
+			elseif child:IsA('Sparkles') then
+				RunService.Heartbeat:Wait()
+				child:Destroy()
+			elseif child:IsA('Smoke') or child:IsA('Fire') then
+				RunService.Heartbeat:Wait()
+				child:Destroy()
+			end
+		end)
+	end)
 end)
 
 MovementSection:NewButton("Inf stamina", "U can run forever", function()
